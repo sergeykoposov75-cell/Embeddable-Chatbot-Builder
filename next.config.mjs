98f0377492d713c98485ee3212c98af9ee32c9f9
sergeykoposov75-cell <sergey.koposov75@gmail.com>
2026-08-03 22:28:21 +0300

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-export default nextConfig;
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
+
+module.exports = nextConfig;
